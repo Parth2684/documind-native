70a5b46df85d693dcb8eab7e5bd6e9766005a09b
Parth2684 <bhosle6006@gmail.com>
2026-05-19 21:51:27 +0530

    feat: add checking file type is pdf ot image

---
+++ src-tauri/src/commands/helpers/file_type.rs
@@ -0,0 +1,32 @@
+use std::path::PathBuf;
+
+use infer::{Infer, MatcherType};
+
+
+enum FileType {
+    Image,
+    PDF
+}
+
+
+pub fn get_file_type(path: &PathBuf) -> Result<FileType, String> {
+    let file_type = infer::get_from_path(path)
+        .map_err(|err| {
+            err.to_string()
+        })?;
+
+    match file_type {
+        None => Err(String::from("Could not infer file type")),
+        Some(filetype) => {
+            if filetype.matcher_type() == MatcherType::Image {
+                return Ok(FileType::Image);
+            }
+            if filetype.mime_type() == "application/pdf" {
+                return Ok(FileType::PDF);
+            }
+            else {
+                return Err(String::from("Unsupported file uploaded"))
+            }
+        }
+    }
+} 
